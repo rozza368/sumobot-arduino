2c590f37865f70f0dcb0062ffe8ab7f10ac1d9fc
rozza368 <rorytheb3st@gmail.com>
2020-11-04 08:26:28 +0800

    initial commit

---
+++ sumobot/sumobot.ino
@@ -0,0 +1,116 @@
+int leftBack = 2;
+int leftFwd = 3;
+int rightBack = 5;
+int rightFwd = 4;
+int shieldRight = 6;
+int shieldLeft = 7;
+int shieldLstate = 0;
+int shieldRstate = 0;
+
+int started = 0;
+
+int motors[4] = {leftBack, leftFwd, rightBack, rightFwd};
+int shields[2] = {shieldRight, shieldLeft};
+
+int ldrPin = A0;
+int ldrVal;
+int ldrThreshold = 650;
+
+
+void getShields() {
+  shieldLstate = digitalRead(shieldLeft);
+  shieldRstate = digitalRead(shieldRight);
+  //Serial.println(shieldLstate);
+  //Serial.println(shieldRstate);
+}
+
+int getLdr() {
+  ldrVal = analogRead(ldrPin);
+  Serial.print("LDR: ");
+  Serial.println(ldrVal);
+  return ldrVal;
+}
+
+void stopped() {
+  // all motors off
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(motors[i], LOW);
+  }
+}
+
+void forward() {
+  digitalWrite(leftFwd, HIGH);
+  digitalWrite(rightFwd, HIGH);
+  digitalWrite(leftBack, LOW);
+  digitalWrite(rightBack, LOW);
+}
+
+void backward() {
+  digitalWrite(leftFwd, LOW);
+  digitalWrite(rightFwd, LOW);
+  digitalWrite(leftBack, HIGH);
+  digitalWrite(rightBack, HIGH);
+}
+
+void pivot() {
+  digitalWrite(leftFwd, HIGH);
+  digitalWrite(rightFwd, LOW);
+  digitalWrite(leftBack, LOW);
+  digitalWrite(rightBack, HIGH);
+  // digitalWrite(rightBack, LOW);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // set up all pins
+  pinMode(leftBack, OUTPUT);
+  pinMode(leftFwd, OUTPUT);
+  pinMode(rightBack, OUTPUT);
+  pinMode(rightFwd, OUTPUT);
+  pinMode(shieldRight, INPUT);
+  pinMode(shieldLeft, INPUT);
+
+  stopped();
+
+  // wait for both shields to be pressed
+  Serial.println("Waiting to start...");
+  do {
+    getShields();
+  } while (shieldLstate || shieldRstate);
+  delay(500); // ensure shield is not detected as hit
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (getLdr() > ldrThreshold) // not over black line
+  {
+    // normal operation
+    forward();
+    getShields();
+    if (!shieldLstate || !shieldRstate) // shield hit
+    {
+      Serial.println("Shield hit, immobilised");
+      // stopped();
+      pivot();
+      delay(1000);
+    }
+  }
+  else  // need to get out of black area
+  {
+    Serial.println("Detected black");
+    // reverse completely out of black
+    backward();
+    while (getLdr() < ldrThreshold)
+    {
+      delay(50);
+    }
+    
+    pivot();
+    delay(400);
+    forward();
+    //delay(200);
+  }
+}
